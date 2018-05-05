1887ae050fbe7616699a216cdaaced84181341bb
Vinicius <viniciusayres.sg@gmail.com>
2018-05-05 12:06:41 -0300

    Mudei a validacao do XML e do XSD. Criei uma função simples pra carregar o XML e o XSD, e deixar o código mais semantico. Pra chamar a validacao tem que usar o load antes.

--- RequestHandler.rb
+++ RequestHandler.rb
@@ -3,25 +3,35 @@ require 'webrick'
 
 class RequestHandler < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(request, response)
-    puts request
+    xml = load_xml(request.body)
+    xsd = load_xsd('myXSD.xsd')
     response.status = 200
   end
 
   def do_POST(request, response)
+    xml = load_xml(request.body)
+    xsd = load_xsd('myXSD.xsd')
     puts request
     response.status = 200
   end
 
   def xml_validation(xml)
-    doc = Nokogiri::XML(xml)
     doc.errors.length == 0
   end
 
-  def xsd_validation(xml)
-    doc = Nokogiri::XML(xml)
-    xsd = Nokogiri::XML::Schema(open('myXSD.xsd'))
-    xsd.validate(doc).length == 0
+  def xsd_validation(xml, xsd)
+    xsd.validate(xml).length == 0
   end
+
+  def load_xml(xml)
+    Nokogiri::XML(xml)
+  end
+
+  def load_xsd(path)
+    Nokogiri::XML::Schema(open(path))
+  end
+
+  private :xml_validation, :xsd_validation, :load_xml, :load_xsd
 end
 
 server = WEBrick::HTTPServer.new(:Port => 5656)
